0ec239c03133e7010a949b1c1ab9eb25dab6aba7
Samuel Ghellere <samuel.ghellere@gmail.com>
2022-10-13 20:06:57 -0300

    Validacao sintática com Bean Validation

--- src/main/java/com/ghellere/cursomc/dto/CategoriaDTO.java
+++ src/main/java/com/ghellere/cursomc/dto/CategoriaDTO.java
@@ -2,12 +2,19 @@ package com.ghellere.cursomc.dto;
 
 import java.io.Serializable;
 
+import javax.validation.constraints.NotEmpty;
+
+import org.hibernate.validator.constraints.Length;
+
 import com.ghellere.cursomc.domain.Categoria;
 
 public class CategoriaDTO implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
 	private Integer id;
+	
+	@NotEmpty(message="Preenchimento obrigatorio")
+	@Length(min = 5, max = 8, message="O tamanho deve ser entre 5 e 80 caracter")
 	private String nome;
 	
 	public CategoriaDTO() {
--- src/main/java/com/ghellere/cursomc/services/CategoriaService.java
+++ src/main/java/com/ghellere/cursomc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.ghellere.cursomc.domain.Categoria;
+import com.ghellere.cursomc.dto.CategoriaDTO;
 import com.ghellere.cursomc.repositories.CategoriaRepository;
 import com.ghellere.cursomc.services.exceptions.DataIntegrityException;
 import com.ghellere.cursomc.services.exceptions.ObjectNotFoundException;
@@ -55,5 +56,9 @@ public class CategoriaService {
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		
 		return repo.findAll(pageRequest);
+	}
+	
+	public Categoria fromDTO(CategoriaDTO objDTO) {
+		return new Categoria(objDTO.getId(),objDTO.getNome());
 	}
 }
