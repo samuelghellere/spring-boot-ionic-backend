da56c05d97e39e6822ed63478cf05fb36d2239cd
Samuel Ghellere <samuel.ghellere@gmail.com>
2022-10-12 22:17:44 -0300

    Projeto Criado

---
+++ src/test/java/com/ghellere/cursomc/CursomcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ghellere.cursomc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursomcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
