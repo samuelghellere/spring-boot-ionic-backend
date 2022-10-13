3601d74b305baba8aea0e54f4d91c040be1db834
Samuel Ghellere <samuel.ghellere@gmail.com>
2022-10-13 08:27:08 -0300

    Ajustes no endpoint /categoriaas/{id}

---
+++ src/main/java/com/ghellere/cursomc/services/exceptions/ObjectNotFoundException.java
@@ -0,0 +1,15 @@
+package com.ghellere.cursomc.services.exceptions;
+
+public class ObjectNotFoundException extends RuntimeException{
+	
+	private static final long serialVersionUID = 1l;
+	
+	public ObjectNotFoundException(String msg) {
+		super(msg);
+	}
+			
+	public ObjectNotFoundException(String msg, Throwable cause) {
+		super(msg, cause);
+	}
+
+}
