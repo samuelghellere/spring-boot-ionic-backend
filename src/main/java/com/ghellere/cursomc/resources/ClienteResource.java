1ad7ceff24392c6b53270d7a8697dedb0292fb3b
Samuel Ghellere <samuel.ghellere@gmail.com>
2022-10-14 10:28:13 -0300

    Vlidacao customizada: CPF ou CNPJ na insercao de Cliente

--- src/main/java/com/ghellere/cursomc/resources/ClienteResource.java
+++ src/main/java/com/ghellere/cursomc/resources/ClienteResource.java
@@ -17,7 +17,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import com.ghellere.cursomc.domain.Categoria;
 import com.ghellere.cursomc.domain.Cliente;
 import com.ghellere.cursomc.dto.ClienteDTO;
 import com.ghellere.cursomc.dto.ClienteNewDTO;
